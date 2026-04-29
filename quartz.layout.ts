4063334c758dd1e137db30b2d2fdc461622cbc2b
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2026-04-30 04:06:47 +0900

    feat(graph): widen local graph to depth 2 for tag-based connectivity

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: { depth: 2 },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
